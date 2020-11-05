ead0f07538a7843aca258b9cd03a967ffced1bee
Matt Jacobs <mattrjacobs@gmail.com>
2020-11-05 06:30:01 -0800

    Added simple blocking HTTP client

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,29 @@
+use std::time::Duration;
+
+use reqwest::blocking::ClientBuilder;
+
 fn main() {
     println!("Hello, world!");
+
+    let http_client = ClientBuilder::default()
+        .connect_timeout(Some(Duration::from_millis(100)))
+        .timeout(Some(Duration::from_millis(1000)))
+        .connection_verbose(true)
+        .build()
+        .expect("Successful client build");
+
+    let http_req = http_client
+        .get("http://httpstat.us/200")
+        .build()
+        .expect("Successful HTTP Request creation");
+
+    let http_resp = http_client
+        .execute(http_req)
+        .expect("Successful HTTP Response");
+
+    println!("HTTP RESP Result : {:?}", http_resp);
+
+    let http_resp_body = http_resp.text().expect("Successful HTTP Response Body");
+
+    println!("HTTP RESP : {}", http_resp_body);
 }
